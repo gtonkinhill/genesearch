75c2919d045fb02f1d71569bf427e91ae5d1fe0f
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2023-05-02 16:03:58 +0000

    code commit

---
+++ genesearch/__main__.py
@@ -0,0 +1,111 @@
+from .summarise import *
+from .search import *
+import yaml
+import importlib
+from importlib.metadata import version
+import sys
+import argparse
+import logging
+
+
+def get_options(args):
+
+    description = 'genesearch: A tool to summarise the literature for a particluar gene in a species.'
+
+    parser = argparse.ArgumentParser(description=description,
+                                     prog='genesearch')
+
+    search_opts = parser.add_argument_group('Input/output')
+    search_opts.add_argument(
+        "-g",
+        "--gene",
+        dest="gene",
+        required=True,
+        help="The gene name.",
+        type=str)
+
+    search_opts.add_argument(
+        "-s",
+        "--species",
+        dest="species",
+        required=True,
+        help="The species name.",
+        type=str)
+
+    search_opts.add_argument("-n",
+                         "--num-papers",
+                         dest="number_papers",
+                         help="Number of papers to summarise (default=3)",
+                         default=3,
+                         type=int)
+
+     # Other options
+    parser.add_argument("--quiet",
+                        dest="verbose",
+                        help="suppress additional output",
+                        action='store_false',
+                        default=True)
+    parser.add_argument('--version',
+                        action='version',
+                        version='%(prog)s ' + version("genesearch"))
+
+    args = parser.parse_args(args)
+    return (args)
+
+
+
+
+
+def main():
+
+    # Load arguments
+    args = get_options(sys.argv[1:])
+
+    # Load API Keys
+    with importlib.resources.as_file(importlib.resources.files("genesearch").joinpath("api_keys.yaml")) as yaml_path:
+        with open(yaml_path) as yaml_file:
+            try:
+                apis = yaml.safe_load(yaml_file)
+            except yaml.YAMLError as error:
+                print(f"Error reading YAML file: {error}")
+
+    openai.api_key = apis['openai_api_key']
+
+    # Define your search query
+    query = f'"{args.gene}" {args.species}'
+
+    # Call the custom Google Search API
+    results = call_google_search_api(apis['google_api_key'], apis['google_engine_id'], query)
+
+    # Download the text of the first 10 results
+    texts = download_text_from_search_results(results, args.number_papers)
+    texts = [text for text in texts if args.gene.lower() in " ".join(text).lower()]
+
+    # split into chuncks and recursively summarise using GPT-3
+    paper_summaries = []
+    for paper in texts:
+        paper_summaries.append(divide_and_conquer_cgpt(paper, args.gene))
+
+    relevant_summaries = []
+    for summary in paper_summaries:
+        if is_species(summary, args.species):
+            relevant_summaries.append(summary)
+
+    if len(relevant_summaries)>1:
+        logging.info("Merging paper summaries")
+        final_summary = divide_and_conquer_cgpt(relevant_summaries, args.gene)
+    elif len(relevant_summaries)==1:
+        logging.info("Only a sinlge paper has been summaried.")
+        final_summary = relevant_summaries[0]
+    else:
+        logging.warning("No relevant paper summaries found!")
+        sys.exit(1)
+
+    print(final_summary)
+
+    return
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ genesearch/summarise.py
@@ -0,0 +1,85 @@
+import openai
+import logging
+
+def call_openai_chat_api(prompt, model="gpt-3.5-turbo"):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        temperature=0
+    )
+    return completion.choices[0].message.content
+
+
+def divide_and_conquer_cgpt(paragraphs, gene):
+
+    summaries = paragraphs
+
+    while len(summaries) > 1:
+        paragraphs = summaries
+        summaries = []
+        i = 0
+        for p in paragraphs:
+            gene_mentioned = gene.lower() in p.lower()
+            response_text = ""
+            has_appended = False
+
+            if i >= 2 and not gene_mentioned: continue
+            # if not gene_mentioned: continue
+
+            # Define your prompt (message)
+            prompt = f'Please summarise the following paragraph in less than 200 words: "{p}".'
+            if gene_mentioned:
+                prompt += f" Focus on the description of the gene {gene} in the paragraph."
+
+            # Call the OpenAI Chat API
+            response_text = call_openai_chat_api(prompt)
+
+            if i%2==1:
+                summaries.append(prev + " " + response_text)
+                has_appended = True
+            else:
+                prev = response_text
+
+            i += 1
+
+            logging.info(f"Summarising paragraph: {i}")
+
+        if not has_appended:
+            summaries[-1] = summaries[-1] + " " + response_text
+
+
+    prompt = f'Please summarise the description of gene {gene} in the following paragraph: "{summaries[0]}".'
+            
+    # Call the OpenAI Chat API
+    response_text = call_openai_chat_api(prompt)
+
+    logging.info("Final paper summary:")
+    logging.info(response_text)
+    
+    return response_text
+
+
+def is_species(text, species):
+
+    prompt = f'Does this paragraph focus on the species {species}? "'
+
+    prompt += text
+
+    prompt += '" Only answer with the words "Yes" or "No" and nothing else'
+    
+    # Call the OpenAI Chat API
+    response_text = call_openai_chat_api(prompt)
+    rtlower = response_text.lower()
+    print(response_text)
+
+    if "no" in rtlower:
+        return False
+    elif "yes" in rtlower:
+        return True
+    else:
+        print("Did not answer with yes or no!")
+        sys.exit(1)
+
+    return None
