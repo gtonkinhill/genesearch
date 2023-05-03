0bf7d0751b55824798a2e3d71af0b3b9fd5cb84c
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2023-05-03 09:20:16 +0000

    formatted with black

--- genesearch/search.py
+++ genesearch/search.py
@@ -2,16 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import logging
 
+
 def call_google_search_api(api_key, engine_id, query):
     # Define the API endpoint
     url = "https://www.googleapis.com/customsearch/v1"
 
     # Define the query parameters
-    params = {
-        "key": api_key,
-        "cx": engine_id,
-        "q": query
-    }
+    params = {"key": api_key, "cx": engine_id, "q": query}
 
     # Make the GET request to the API
     response = requests.get(url, params=params)
@@ -29,12 +26,12 @@ def call_google_search_api(api_key, engine_id, query):
 def download_text_from_url(url, min_word_count=100):
     try:
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"
         }
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            paragraphs = soup.find_all('p')
+            soup = BeautifulSoup(response.content, "html.parser")
+            paragraphs = soup.find_all("p")
             long_paragraphs = []
             for paragraph in paragraphs:
                 text = paragraph.get_text()
@@ -51,12 +48,13 @@ def download_text_from_url(url, min_word_count=100):
         logging.error(f"Error downloading text from URL: {url}. Error: {e}")
         return None
 
+
 def download_text_from_search_results(results, num_results=10):
     if results:
-        items = results.get('items', [])
+        items = results.get("items", [])
         texts = []
         for index, item in enumerate(items[:num_results]):
-            url = item.get('link')
+            url = item.get("link")
             logging.info(f"Downloading text from result {index + 1}: {url}")
             text = download_text_from_url(url)
             if text:
